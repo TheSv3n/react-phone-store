08d03a17575488ddd59079c61491b83912aaf4a9
Steven Creighton <ste.creighton@googlemail.com>
2019-02-22 17:55:04 +0000

    temp commit

--- src/components/Cart/CartTotals.js
+++ src/components/Cart/CartTotals.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import PayPalButton from "./PayPalButton";
 
 export default function CartTotals({ value }) {
   const { cartSubTotal, cartTax, cartTotal, clearCart } = value;
